0ad236c550c6060490666ce7108e02209b4aed06
Kevien Damico <keviendamico@outlook.it>
2026-06-05 16:20:00 +0200

    fix: env separation

---
+++ src/main/resources/db/prod/V2__seed_admin.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (username, email, password_hash, role, enabled)
+VALUES (
+    'kappanove',
+    'kappanove@fantaschedina.dev',
+    '$2y$12$jAQ1Fuw9lwkM/putPjAS4.7krHOc2r/Y3jDRlRXfcuJ/0Pz0AHxkG',
+    'ADMIN',
+    TRUE
+);
