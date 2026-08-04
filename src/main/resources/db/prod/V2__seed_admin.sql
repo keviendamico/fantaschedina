02a23724a6969f8fe75f8bf03754c741b5cbda9c
Kevien Damico <damicokevien@gmail.com>
2026-08-04 22:13:55 +0200

    fix: various

--- src/main/resources/db/prod/V2__seed_admin.sql
+++ src/main/resources/db/prod/V2__seed_admin.sql
@@ -2,7 +2,7 @@ INSERT INTO users (username, email, password_hash, role, enabled)
 VALUES (
     'kappanove',
     'kappanove@fantaschedina.dev',
-    '$2y$12$jAQ1Fuw9lwkM/putPjAS4.7krHOc2r/Y3jDRlRXfcuJ/0Pz0AHxkG',
+    '$2b$12$szLjkrl4RDt0WgHOSY4yBukwdcjH2xLDJaYLox5ZrI.NX4tGNZhwm',
     'ADMIN',
     TRUE
 );
